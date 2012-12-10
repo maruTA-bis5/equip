a73001dfcfb238707abe02469da5cec65106d1a9
bis5 <s10149tm@edu.tuis.ac.jp>
2012-12-10 10:08:38 +0900

    個別備品情報画面に備品種別を表示

--- xoops_trust_path/modules/equip/actions/ItemsViewAction.class.php
+++ xoops_trust_path/modules/equip/actions/ItemsViewAction.class.php
@@ -11,6 +11,7 @@ if(!defined('XOOPS_ROOT_PATH'))
 }
 
 require_once EQUIP_TRUST_PATH . '/class/AbstractViewAction.class.php';
+require_once EQUIP_TRUST_PATH . '/actions/TypesListAction.class.php';
 
 /**
  * Equip_ItemsViewAction
@@ -44,6 +45,16 @@ class Equip_ItemsViewAction extends Equip_AbstractViewAction
 	**/
 	public function executeViewSuccess(/*** XCube_RenderTarget ***/ &$render)
 	{
+        $types = new Equip_TypesListAction;
+        $types->execute();
+        $tobj = $types->mObjects;
+        $tid = $this->mObject->get('types_id');
+        foreach ($tobj as $obj) {
+            if ($obj->get('types_id') == $tid) {
+                $render->setAttribute('typename', $obj->get('name'));
+                break;
+            }
+        }
 		$render->setTemplateName($this->mAsset->mDirname . '_items_view.html');
 		$render->setAttribute('object', $this->mObject);
 		$render->setAttribute('dirname', $this->mAsset->mDirname);
