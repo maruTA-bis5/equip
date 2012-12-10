ed3be2f73fccae295ae91583a0a20fb7bb207352
bis5 <s10149tm@edu.tuis.ac.jp>
2012-12-10 09:57:42 +0900

    備品情報編集画面で種別を選択できる実装

--- xoops_trust_path/modules/equip/actions/ItemsEditAction.class.php
+++ xoops_trust_path/modules/equip/actions/ItemsEditAction.class.php
@@ -11,6 +11,7 @@ if(!defined('XOOPS_ROOT_PATH'))
 }
 
 require_once EQUIP_TRUST_PATH . '/class/AbstractEditAction.class.php';
+require_once EQUIP_TRUST_PATH . '/actions/TypesListAction.class.php';
 
 /**
  * Equip_ItemsEditAction
@@ -57,9 +58,21 @@ class Equip_ItemsEditAction extends Equip_AbstractEditAction
     **/
     public function executeViewInput(/*** XCube_RenderTarget ***/ &$render)
     {
+        $tact = new Equip_TypesListAction;
+        $tact->execute();
+        $types = $tact->mObjects;
+        $typelist = array();
+        foreach ($types as $type) {
+            $obj = new XoopsSimpleObject;
+            $obj->initVar('name', XOBJ_DTYPE_STRING, $type->get('name'));
+            $obj->initVar('value', XOBJ_DTYPE_INT, $type->get('types_id'));
+            $typelist[$type->get('types_id')] = $obj;
+        }
         $render->setTemplateName($this->mAsset->mDirname . '_items_edit.html');
         $render->setAttribute('actionForm', $this->mActionForm);
         $render->setAttribute('object', $this->mObject);
+        $render->setAttribute('typelist', $typelist);
+        $render->setAttribute('type_default', $this->mObject->get('types_id'));
         $render->setAttribute('dirname', $this->mAsset->mDirname);
         $render->setAttribute('dataname', self::DATANAME);
 
